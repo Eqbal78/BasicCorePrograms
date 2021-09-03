3910e45de9c9c54b37b233d706f9c43b034675df
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-09-03 18:46:01 +0530

    Percantage of Heads and Tails

---
+++ FlipCoin/FlipCoin/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace FlipCoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Basic Core Program");
+
+            Random Rand = new Random();
+
+            int heads = 0;
+            int tails = 0;
+            Double percentage = 0;
+            int result = 0;
+            Console.WriteLine("Number of time flip coin");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < num; i++)
+            {
+                result = Rand.Next(0, 2);
+
+                if (result == 1)
+                {
+                    heads++;
+                }
+                else
+                {
+                    tails++;
+                }
+
+            }
+            Console.WriteLine("heads was flipped {0} times", heads);
+            percentage = ((heads*100d) / num);
+            Console.WriteLine("Percentage: " + percentage);
+            Console.WriteLine("tails was flipped {0} times", tails);
+            percentage = ((tails * 100d) / num);
+            Console.WriteLine("Percentage: " + percentage);
+        }
+    }
+}
