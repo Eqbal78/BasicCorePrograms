1b751d0fb8c124e1efc9b81f592314601e943749
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-09-03 22:07:11 +0530

    Check Wheather Number is Even or Odd

---
+++ EvenOrOdd/EvenOrOdd/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EvenOrOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Number");
+            int num = int.Parse(Console.ReadLine());
+            if (num%2 == 0)
+            {
+                Console.WriteLine("It is an Even Number.");
+            }
+            else
+            {
+                Console.WriteLine("It is an Odd Number.");
+            }
+        }
+    }
+}
