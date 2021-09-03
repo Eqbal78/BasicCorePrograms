245d2318ed50a36e35e8c0eb3c7818d7f4b17250
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-09-03 20:31:23 +0530

    Sum of Harmonic Series

---
+++ HarmonicNumber/HarmonicNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HarmonicNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n;
+            float s = 0;
+
+            Console.Write("Input the number of terms : ");
+            n = int.Parse(Console.ReadLine());
+            Console.Write("\n");
+            for (i = 1; i <= n; i++)
+            {
+                Console.Write("1/{0} + ", i);
+                s += 1 /(float)i;
+            }
+            Console.Write("\nSum of Series upto {0} terms : {1}\n ", n, s);
+        }
+    }
+}
