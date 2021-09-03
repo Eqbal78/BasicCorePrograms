652d88e306e3019d28de8465bde5db21d75ad128
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-09-03 20:10:00 +0530

    Table of the Power of Two

---
+++ PowerOfTwo/PowerOfTwo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PowerOfTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Number:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Double num = 0;
+
+            for (int i=1; i<=n; i++)
+            {
+                num = Math.Pow(i, 2);
+                Console.WriteLine("Power of " +i+ " is: " +num);
+            }
+          
+        }
+    }
+}
