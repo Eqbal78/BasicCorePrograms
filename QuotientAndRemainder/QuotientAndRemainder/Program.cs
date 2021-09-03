283dba40b81c55d45269519b79123877d433aee7
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-09-03 21:39:41 +0530

    Divide the number and get the Quotient and Remainder

---
+++ QuotientAndRemainder/QuotientAndRemainder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace QuotientAndRemainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the Dividend ");
+            int dividend = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the Divisor ");
+            int divisor = int.Parse(Console.ReadLine());
+
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+        }
+    }
+}
